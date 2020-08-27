0193cb341008c76f6b8e5b06cc535a915ce26d15
Jezza <jezzadabomb@gmail.com>
2020-08-28 00:26:49 +0200

    Add ability to configure extension directly, rather than using properties.
    If you declare the extension in the plugin section, with the extension value, you can define a configuration section.
    
    The extension now looks for this configuration section, and configures itself from it.
    
    If it can't find it, it still defaults to looking up properties.

--- src/main/java/com/jezza/github/ReactorLifecycle.java
+++ src/main/java/com/jezza/github/ReactorLifecycle.java
@@ -3,11 +3,14 @@ package com.jezza.github;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.maven.AbstractMavenLifecycleParticipant;
 import org.apache.maven.MavenExecutionException;
 import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.Build;
+import org.apache.maven.model.Plugin;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.ProjectBuilder;
 import org.apache.maven.project.ProjectBuildingException;
@@ -16,6 +19,7 @@ import org.apache.maven.project.ProjectBuildingResult;
 import org.codehaus.plexus.component.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
 import org.codehaus.plexus.logging.Logger;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
 
 @Component(role = AbstractMavenLifecycleParticipant.class, hint = "reactor")
 public class ReactorLifecycle extends AbstractMavenLifecycleParticipant {
@@ -40,26 +44,20 @@ public class ReactorLifecycle extends AbstractMavenLifecycleParticipant {
 		}
 		MavenProject project = session.getCurrentProject();
 
+		String[] children = determineChildren(project);
+		if (children == null || children.length == 0) {
+			return;
+		}
+
 		File base = project.getFile().getParentFile();
 
-		String property = project.getProperties().getProperty(CHILDREN_PROP);
-		if (property == null || property.isEmpty()) {
-			log.info("No \"" + CHILDREN_PROP + "\" property set; Doing nothing...");
-			return;
-		}
-
-		String[] files = property.split("\n");
-		if (files.length == 0) {
-			return;
-		}
-
-		for (String file : files) {
-			file = file.trim();
-			if (file.isEmpty()) {
+		for (String child : children) {
+			child = child.trim();
+			if (child.isEmpty()) {
 				continue;
 			}
 
-			File target = findTarget(base, file);
+			File target = findTarget(base, child);
 			if (target == null) {
 				continue;
 			}
@@ -75,6 +73,44 @@ public class ReactorLifecycle extends AbstractMavenLifecycleParticipant {
 		}
 	}
 
+	private String[] determineChildren(MavenProject project) {
+		Build build = project.getBuild();
+		Plugin plugin = build.getPluginsAsMap().get("com.github.jezza:simplified-reactor-plugin");
+
+		Object configuration = plugin.getConfiguration();
+		if (configuration instanceof Xpp3Dom) {
+			Xpp3Dom config = (Xpp3Dom) configuration;
+
+			String[] children = extract(config);
+			if (children != null) {
+				return children;
+			}
+		}
+
+		String property = project.getProperties().getProperty(CHILDREN_PROP);
+		if (property == null || property.isEmpty()) {
+			log.info("No \"" + CHILDREN_PROP + "\" property set; Doing nothing...");
+			return null;
+		}
+
+		return property.split("\n");
+	}
+
+	private static final String[] EMPTY = new String[0];
+
+	private String[] extract(Xpp3Dom config) {
+		Xpp3Dom[] children = config.getChildren("child");
+		if (children == null || children.length == 0) {
+			return null;
+		}
+
+		List<String> paths = new ArrayList<>(children.length);
+		for (Xpp3Dom child : children) {
+			paths.add(child.getValue().trim());
+		}
+		return paths.toArray(EMPTY);
+	}
+
 	private File findTarget(File base, String path) {
 		if (path == null || path.isEmpty()) {
 			return null;
@@ -84,13 +120,27 @@ public class ReactorLifecycle extends AbstractMavenLifecycleParticipant {
 				.resolve(path)
 				.normalize();
 
-		if (Files.isDirectory(resolved)) {
-			resolved = resolved.resolve("pom.xml");
+		if (testPath(resolved)) {
+			return resolved.toFile();
 		}
-		if (!Files.exists(resolved)) {
-			log.warn("File/Directory does not exist: " + resolved);
-			return null;
+
+		resolved = resolved.getParent();
+		if (testPath(resolved)) {
+			return resolved.toFile();
 		}
-		return resolved.toFile();
+
+		log.warn("Unable to locate pom.xml at " + path);
+		return null;
+	}
+
+	private boolean testPath(Path path) {
+		if (Files.isDirectory(path)) {
+			path = path.resolve("pom.xml");
+		}
+		if (!Files.exists(path)) {
+			log.warn("File/Directory does not exist: " + path);
+			return false;
+		}
+		return true;
 	}
 }
